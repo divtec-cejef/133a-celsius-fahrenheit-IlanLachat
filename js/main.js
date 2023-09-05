dfd8b57c3a1be3282f130087d7abed79386b6417
lachila <ilan.lachat@divtec.ch>
2023-09-05 11:41:07 +0200

    Nettoyage main-js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@
  * @since   2019-08-19
  */
 
-(function main() {
+
   'use strict'; // Demande un interprétation stricte du code
 
   let a = 'Bonjour';
 
   console.log(a);
-}()); // Main IIFE
+
 
